048fab92be068323f4fc0f83d87bc3755c66b58c
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2022-11-09 13:17:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2888,7 +2888,7 @@ function loadData() {
         result.gyms.forEach(function(item) {
           gyms.push(item);
           if (settings.showUnknownPois == false) {
-            let lastUpdate = new Date(item.updated*1000).toUTCString().slice(4,-4);
+            let lastUpdate = new Date(item.last_seen*1000).toUTCString().slice(4,-4);
             let radius = (6/8) + ((7/8) * (map.getZoom() - 11)) // Depends on Zoomlevel
             let weight = (1/8) + ((1/8) * (map.getZoom() - 11)) // Depends on Zoomlevel
             if(item.ex == 1){
@@ -2918,7 +2918,7 @@ function loadData() {
             marker.bindPopup("<span>ID: " + item.id + "<br>" + item.name + "<br>" + subs.lastUpdate + lastUpdate + "</span>").addTo(gymLayer);
             }
           } else if (settings.showUnknownPois == true && item.name == null) {
-            let lastUpdate = new Date(item.updated*1000).toUTCString().slice(4,-4);
+            let lastUpdate = new Date(item.last_seen*1000).toUTCString().slice(4,-4);
             let radius = (6/8) + ((7/8) * (map.getZoom() - 11)) // Depends on Zoomlevel
             let weight = (1/8) + ((1/8) * (map.getZoom() - 11)) // Depends on Zoomlevel
             if(item.ex == 1){
@@ -2955,7 +2955,7 @@ function loadData() {
           if (item.deleted != 1) {
           if (settings.showUnknownPois == false) {
             pokestops.push(item);
-            let lastUpdate = new Date(item.updated*1000).toUTCString().slice(4,-4);
+            let lastUpdate = new Date(item.last_seen*1000).toUTCString().slice(4,-4);
             let radius = (6/8) + ((6/8) * (map.getZoom() - 11)) // Depends on Zoomlevel
             let weight = (1/8) + ((1/8) * (map.getZoom() - 11)) // Depends on Zoomlevel
             let marker = L.circleMarker([item.lat, item.lng], {
@@ -2971,7 +2971,7 @@ function loadData() {
             marker.bindPopup("<span>ID: " + item.id + "<br>" + item.name + "<br>" + subs.lastUpdate + lastUpdate + "</span>").addTo(pokestopLayer);
           } else if (settings.showUnknownPois == true && item.name == null) {
             pokestops.push(item);
-            let lastUpdate = new Date(item.updated*1000).toUTCString().slice(4,-4);
+            let lastUpdate = new Date(item.last_seen*1000).toUTCString().slice(4,-4);
             let radius = (6/8) + ((6/8) * (map.getZoom() - 11)) // Depends on Zoomlevel
             let weight = (1/8) + ((1/8) * (map.getZoom() - 11)) // Depends on Zoomlevel
             let marker = L.circleMarker([item.lat, item.lng], {
@@ -3022,16 +3022,16 @@ function loadData() {
         if (settings.hideOldSpawnpoints == true){
           let oldSpawnpointsTimestamp = settings.oldSpawnpointsTimestamp;
           result.spawnpoints.forEach(function(item) {
-            if (item.despawn_sec != null && item.updated >= oldSpawnpointsTimestamp) {
+            if (item.despawn_sec != null && item.last_seen >= oldSpawnpointsTimestamp) {
               spawnpoints.push(item);
-            } else if (item.updated >= oldSpawnpointsTimestamp){
+            } else if (item.last_seen >= oldSpawnpointsTimestamp){
               spawnpoints_u.push(item);
               spawnpoints.push(item);
             }
             let radius = (6/8) + ((4/8) * (map.getZoom() - 11)) // Depends on Zoomlevel
             let weight = (1/8) + ((1/8) * (map.getZoom() - 11)) // Depends on Zoomlevel
             if (settings.showUnknownPois == false){
-              if (item.despawn_sec != null && item.updated >= oldSpawnpointsTimestamp) {
+              if (item.despawn_sec != null && item.last_seen >= oldSpawnpointsTimestamp) {
                 let marker = L.circleMarker([item.lat, item.lng], {
                   color: 'black',
                   fillColor: 'blue',
@@ -3044,7 +3044,7 @@ function loadData() {
                 marker.tags.id = item.id;
                 let despawn_time = new Date(parseInt(item.despawn_sec)*1000).toISOString().slice(-10, -5);
                 marker.bindPopup("<span>ID: " + item.id + "</span>\n" + subs.despawnTime + despawn_time).addTo(spawnpointLayer);
-              } else if (item.despawn_sec == null && item.updated >= oldSpawnpointsTimestamp) {
+              } else if (item.despawn_sec == null && item.last_seen >= oldSpawnpointsTimestamp) {
                 let marker = L.circleMarker([item.lat, item.lng], {
                   color: 'black',
                   fillColor: 'red',
@@ -3057,7 +3057,7 @@ function loadData() {
                 marker.tags.id = item.id;
                 marker.bindPopup("<span>ID: " + item.id + "</span>\n" + subs.unknownDespawnTime).addTo(spawnpointLayer);
               }
-            } else if (settings.showUnknownPois == true && item.despawn_sec == null && item.updated >= oldSpawnpointsTimestamp){
+            } else if (settings.showUnknownPois == true && item.despawn_sec == null && item.last_seen >= oldSpawnpointsTimestamp){
               let marker = L.circleMarker([item.lat, item.lng], {
                   color: 'black',
                   fillColor: 'red',
@@ -5282,7 +5282,7 @@ function getData($args) {
   }
 
   if ($args->show_spawnpoints === true) {
-    $sql_spawnpoint = "SELECT id, despawn_sec, lat, lon as lng, updated FROM spawnpoint WHERE lat > ? AND lon > ? AND lat < ? AND lon < ?";
+    $sql_spawnpoint = "SELECT id, despawn_sec, lat, lon as lng, last_seen FROM spawnpoint WHERE lat > ? AND lon > ? AND lat < ? AND lon < ?";
     $stmt = $db->prepare($sql_spawnpoint);
     $stmt->execute([$args->min_lat, $args->min_lng, $args->max_lat, $args->max_lng]);
     $spawns = $stmt->fetchAll(PDO::FETCH_ASSOC);
